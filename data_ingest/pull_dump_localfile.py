77c9050da10f7fcef5f9e285cb5c559243ce51f2
OKlein11 <kleinmowen@gmail.com>
2025-12-15 19:53:12 -0600

    Add gitignore. Remove network data pull, add localfile data pull

---
+++ data_ingest/pull_dump_localfile.py
@@ -0,0 +1,15 @@
+## For pulling the /aircraft.json file over the network then passing it to the flask app
+import requests
+import os
+import json
+
+def main():
+    with open(os.path.join("run","dump1090-fa","aircraft.json")) as f:
+        data = json.load(f)
+    
+    requests.post("127.0.0.1:5000/add_dump", data=data)
+
+
+
+if __name__ == "__main__":
+    main()
